851f0e3ed4a444613d28a2147c45c315b8a32e27
bagasadit123 <bagasadit430@gmail.com>
2026-07-22 10:49:01 +0700

    server setelah koneksi database berhasil

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const connectDatabase = require("./config/db");
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
+
+startServer();
